f721b62e8c32b5827c9e3cd482f0d0f7792c1ebd
Lennie Budgell <868085+Bedrovelsen@users.noreply.github.com>
2022-12-27 22:58:45 -0600

    Update asn2nriched.sh
    Fixed typo in mapcidr flags
    Added input search query organizations result as file output
    Changed cidr range output from rangz to cidrz

--- asn2nriched.sh
+++ asn2nriched.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nrich -o json <(mapcidr -l <(grep -Ei "$1" asns.csv | grep -Ei -o '^AS[0-9]*' | sed 's#AS##g' | xargs -I {} grep -Ei ' {}$' table.txt | grepaddr -cidr4)) | tee rangz.txt | tee asnriched.json
+nrich -o json <(mapcidr -cl <(grep -Ei "$1" asns.csv | tee orgz.txt | grep -Ei -o '^AS[0-9]*' | sed 's#AS##g' | xargs -I {} grep -Ei ' {}$' table.txt | grepaddr -cidr4)) | tee cidrz.txt | tee asnriched.json
