d863b01a9db797a335de132187108d2d506d7f0a
Lennie Budgell <868085+Bedrovelsen@users.noreply.github.com>
2022-12-27 23:50:54 -0600

    Update asn2nriched.sh

--- asn2nriched.sh
+++ asn2nriched.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nrich -o json <(mapcidr -cl <(grep -Ei "$1" asns.csv | tee orgz.txt | grep -Ei -o '^AS[0-9]*' | sed 's#AS##g' | xargs -I {} grep -Ei ' {}$' table.txt | grepaddr -cidr4)) | tee cidrz.txt | tee asnriched.json
+nrich -o json <(mapcidr -silent -cl <(grep -Ei "$1" asns.csv | tee orgz.txt | grep -Ei -o '^AS[0-9]*' | sed 's#AS##g' | xargs -I {} grep -Ei ' {}$' table.txt | grepaddr -cidr4 | tee cidrz.txt) | tee ipz.txt) | tee nrichedASNz.json
