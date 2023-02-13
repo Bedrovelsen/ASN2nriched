5215b6b0ed82973ced306db59e04d3ee128b787c
Lennie Budgell <868085+Bedrovelsen@users.noreply.github.com>
2023-02-13 03:40:08 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,25 +19,4 @@ echo "installing mapcidr"
 
 go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
 
-echo "setup finished"#! /bin/bash
-
-echo "downloading table.txt and asns.csv from bgp.tools..."
-
-wget https://bgp.tools/table.txt
-wget https://bgp.tools/asns.csv
-
-echo "...done"
-
-echo "installing grepaddr"
-
-git clone https://github.com/Zarcolio/grepaddr
-cd grepaddr
-sudo bash install.sh
-cd ..
-
-echo "done"
-echo "installing mapcidr"
-
-go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
-
 echo "setup finished"
